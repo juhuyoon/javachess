953016aed74f4e3adba483251ca7305f745a6186
Jung Hoon Yoon <juhuyoon@yahoo.com>
2018-07-01 22:02:54 -0400

    random

--- javachess/src/com/chess/engine/board/Board.java
+++ javachess/src/com/chess/engine/board/Board.java
@@ -2,6 +2,7 @@ package com.chess.engine.board;
 
 import com.chess.engine.Alliance;
 import com.chess.engine.pieces.*;
+import com.chess.engine.player.BlackPlayer;
 import com.chess.engine.player.WhitePlayer;
 import com.google.common.collect.ImmutableList;
 
