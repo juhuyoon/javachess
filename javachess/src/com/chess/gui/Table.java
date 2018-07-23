dfcd71ae4bcb4893868b60fdcf3cc17609f11f1b
Jung Hoon Yoon <juhuyoon@yahoo.com>
2018-07-22 23:35:03 -0400

    GUI

--- javachess/src/com/chess/gui/Table.java
+++ javachess/src/com/chess/gui/Table.java
@@ -1,28 +1,39 @@
 package com.chess.gui;
 
 
+import com.chess.engine.board.BoardUtils;
+
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Table {
 
     private final JFrame gameFrame;
+    private final BoardPanel boardPanel;
+    private final static Dimension OUTER_FRAME_DIMENSION = new Dimension(600, 600);
+    private final static Dimension BOARD_PANEL_DIMENSION = new Dimension(400, 350);
+    private final static Dimension TILE_PANEL_DIMENSION = new Dimension(10,10);
 
-    private static Dimension OUTER_FRAME_DIMENSION = new Dimension(600,600);
-
+    /* making the menu*/
     public Table() {
         this.gameFrame = new JFrame("Chess with Java");
-        final JMenuBar tableMenuBar = new JMenuBar();
-        populateMenuBar(tableMenuBar);
+        this.gameFrame.setLayout(new BorderLayout());
+        final JMenuBar tableMenuBar = createTableMenuBar();
         this.gameFrame.setJMenuBar(tableMenuBar);
         this.gameFrame.setSize(OUTER_FRAME_DIMENSION);
+        this.boardPanel = new BoardPanel();
+        this.gameFrame.add(this.boardPanel, BorderLayout.CENTER);
         this.gameFrame.setVisible(true);
     }
 
-    private void populateMenuBar(final JMenuBar tableMenuBar) {
+    private JMenuBar createTableMenuBar() {
+        final JMenuBar tableMenuBar = new JMenuBar();
         tableMenuBar.add(createFileMenu());
+        return tableMenuBar;
     }
 
     private JMenu createFileMenu() {
@@ -30,13 +41,47 @@ public class Table {
         /*PGN files lookup! */
         final JMenuItem openPGN = new JMenuItem("Load PGN File");
         openPGN.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                    System.out.println("Open up the PGN File");
-            }
-        });
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            System.out.println("Open up the PGN File");
+        }
+    });
         fileMenu.add(openPGN);
         return fileMenu;
+}
+
+private class BoardPanel extends JPanel {
+    final List<TilePanel> boardTiles;
+
+    BoardPanel() {
+        super(new GridLayout(8,8));
+        this.boardTiles = new ArrayList<>();
+        for(int i = 0; i < BoardUtils.NUM_TILES; i++) {
+            final TilePanel tilePanel = new TilePanel(this, i); //creates indv. tile panel and the tileId of i.
+            this.boardTiles.add(tilePanel);
+            add(tilePanel);
+        }
+        setPreferredSize(BOARD_PANEL_DIMENSION);
+        validate();
     }
+}
+
+private class TilePanel extends JPanel{
+        private final int tileId;
+
+        TilePanel(final BoardPanel boardPanel, final int tileId) {
+            super (new GridBagLayout());
+            this.tileId = tileId;
+            setPreferredSize(TILE_PANEL_DIMENSION);
+            assignTileColor();
+            validate();
+
+        }
+
+    private void assignTileColor() {
+
+    }
+
+}
 
 }
